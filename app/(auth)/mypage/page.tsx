9ab1e9b461aeaa4bc3a9a9b10da173e1208f983e
Sophia <suna9267@gmail.com>
2024-07-16 19:01:16 +0900

    add: mypage 퍼블리싱 초안

--- app/(auth)/mypage/page.tsx
+++ app/(auth)/mypage/page.tsx
@@ -1,12 +1,19 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 import Image from "next/image";
+import { COUNTRIES } from '../../../lib/countries';
+import { SelectMenuOption } from "../../../lib/types";
+import CountrySelector from "../../../components/selector";
 
 export default function MyPage() {
-  const router = useRouter();
+  const myRef = React.createRef<HTMLDivElement>();
+
+  const [isOpen, setIsOpen] = useState(false);
+  // Default this to a country's code to preselect it
+  const [country, setCountry] = useState('AF');
 
   return (
     <div className="flex flex-col items-center justify-center h-full w-full">
@@ -31,9 +38,8 @@ export default function MyPage() {
         </Link>
       </div>
       <div className="text-2xl font-bold mb-10">My Page</div>
-      <div className="flex flex-col justify-center items-center border border-gray-300 mx-8 h-3/4 w-3/4 md:h-32">
-        <div className="flex flex-row items-end">
-          <div className="text-base font-semibold mr-5">Sophia</div>
+      <div className="flex flex-col items-center border border-gray-300 py-10 px-10 mx-8 h-3/4 min-h-72 w-3/4">
+        <div className="flex flex-col items-center justify-center mb-6">
           <Image
             src="/assets/profile.svg"
             alt="logo"
@@ -41,20 +47,48 @@ export default function MyPage() {
             height={90}
             className="transform scale-110"
           />
+          <div className="text-base font-semibold mt-4">Sophia</div>
         </div>
-        <div className="flex flex-row items-end">
+        <div className="w-full h-16 flex flex-row justify-center items-center px-3">
+          <label className="w-1/4 block align-middle text-base font-semibold text-gray-900 dark:text-white" htmlFor="user_avatar">Upload file</label>
+          <form className="w-4/6">
+            <input className="block w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" aria-describedby="user_avatar_help" id="user_avatar" type="file" />
+          </form>
+        </div>
+        <div className="w-full h-16 flex flex-row justify-center items-center px-3">
           <label
             htmlFor="first_name"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            className="w-1/4 align-middle block text-base font-semibold font-medium text-gray-800 dark:text-white"
           >
             Nickname
           </label>
           <input
             type="text"
             id="first_name"
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            className="w-3/6 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             placeholder="John"
           />
+          <button className="w-1/6 ml-1 text-base font-semibold bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">CHECK</button>
+        </div>
+        <div className="w-full h-16 flex flex-row justify-center items-center px-3">
+          <label htmlFor="countries" className="block w-1/4 text-base font-semibold text-gray-900 dark:text-white">Country</label>
+          <CountrySelector
+            id={'countries'}
+            open={isOpen}
+            onToggle={() => setIsOpen(!isOpen)}
+            onChange={val => setCountry(val)}
+            // We use this type assertion because we are always sure this find will return a value but need to let TS know since it could technically return null
+            selectedValue={COUNTRIES.find(option => option.value === country) as SelectMenuOption} 
+          />
+        </div>
+        <div className="w-full h-40 flex flex-row justify-center items-center px-3">
+          <form className="w-full flex flex-row justify-center items-center">
+            <label htmlFor="message" className="w-1/4 block mb-2 text-base font-semibold text-gray-900 dark:text-white">Your message</label>
+            <textarea id="message" rows={4} className="w-4/6 h-36 block p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
+          </form>
+        </div>
+        <div className="w-full h-4 flex flex-row justify-center items-center px-3 mt-14">
+        <button className="w-1/6 ml-1 text-base font-semibold bg-gray-50 border border-gray-300 text-gray-800 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white">SAVE</button>
         </div>
       </div>
     </div>
